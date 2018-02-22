b09b78c06fef3d754a6494f881f6382bb8fa8c24
RealToughCandy <barncatdigital@gmail.com>
2018-02-22 13:23:49 -0600

    Add files via upload

---
+++ peehp.php
@@ -0,0 +1,28 @@
+<DOCTYPE html>
+  <html lang='en'>
+  <head>
+    <meta charset='UFT-8'>
+    <title> PeaAiechPea </title>
+  </head>
+  <body>
+    <?php $header_title = 'real tuff candy wuz here' ?>
+
+<div style='color: indianred; font-size: 8vw'><?php echo $header_title ?></div>
+<?php echo '<h1> hello whassup</h1>' ?>
+
+<br>
+<?php
+$oh_hello= array(20, 30, 900); ?>
+
+<h1><?php echo $oh_hello[0]; ?><h1>
+
+
+  <?php
+  $oh_hello_again = array('first_name' => 'Maddy', 'last_name' => 'DeJesus'); ?>
+  <br>
+  <?php
+  echo '<h1> my name is: ' . $oh_hello_again['first_name'] . ' ' . $oh_hello_again['last_name']; '<h1>' ?>
+
+
+  </body>
+  </html>
